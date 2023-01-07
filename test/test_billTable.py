2f91bd94edada0f248765874b4e9fc245482ddae
Maximilian Kraft <kraft@spengergasse.at>
2023-01-07 16:03:13 +0100

    [test] added test for number at the frint of product name

--- test/test_billTable.py
+++ test/test_billTable.py
@@ -9,7 +9,25 @@ def test_parseBills():
     assert len(bills) == 11
 
 
-def test_spaceAndNumericValueInProductList():
+def test_normalProductListNoNumericValue():
+    bills = parseTxtFile(testfile)
+
+    expectedDict = {
+        'Beschreibung': 'RECHNUNG',
+        'Nr.': 2,
+        'ID-Nr.': 2,
+        'Datum': '03.05.2016',
+        'Produkt': 'Fuï¿½pflege_2023',
+        'Anzahl': 1,
+        'Einzelpreis': 28.0,
+        'Gesamtpreis': 28.0
+    }
+
+    dictList: [{}] = transformBillsToDictList(bills)
+
+    assert dictList[1]["Produkt"] == expectedDict["Produkt"]
+
+def test_spaceAndNumericValueAtEndInProductList():
     bills = parseTxtFile(testfile)
 
     expectedDict = {
@@ -26,3 +44,22 @@ def test_spaceAndNumericValueInProductList():
     dictList: [{}] = transformBillsToDictList(bills)
 
     assert dictList[0]["Produkt"] == expectedDict["Produkt"]
+
+
+def test_spaceAndNumericValueAtBeginInProductList():
+    bills = parseTxtFile(testfile)
+
+    expectedDict = {
+        'Beschreibung': 'RECHNUNG',
+        'Nr.': 1,
+        'ID-Nr.': 1,
+        'Datum': '03.05.2016',
+        'Produkt': '2023 Fuï¿½pflege+Lack',
+        'Anzahl': 1,
+        'Einzelpreis': 29.0,
+        'Gesamtpreis': 29.0
+    }
+
+    dictList: [{}] = transformBillsToDictList(bills)
+
+    assert dictList[2]["Produkt"] == expectedDict["Produkt"]
